8c908bf5947d7cfed3e972742dcaa32ac67030d4
stefnotch <brandmairstefan@gmail.com>
2022-07-22 10:34:14 +0200

    Scaffold some organizer event pages

---
+++ frontend/src/components/EditEvent.vue
@@ -0,0 +1,2 @@
+<template>Component for editing events.</template>
+<script setup lang="ts"></script>
---
+++ frontend/src/pages/EventEditPage.vue
@@ -0,0 +1,8 @@
+<template>
+  Page for creating/editing events. It'll edit an event if one is specified in the URL.
+
+  <EditEvent />
+</template>
+<script setup lang="ts">
+import EditEvent from "@/components/EditEvent.vue";
+</script>
