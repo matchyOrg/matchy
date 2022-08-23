c6122c458e07680f82b665ddf51575b247ffa375
stefnotch <brandmairstefan@gmail.com>
2022-08-23 17:56:33 +0200

    Remove unused dependency

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -20,18 +20,7 @@ import { vuetify } from "@/plugins/vuetify";
 
 // pinia
 import { createPinia } from "pinia";
-import { PiniaSharedState } from "pinia-shared-state";
 const pinia = createPinia();
-pinia.use(
-  PiniaSharedState({
-    // If set to true, enabled for all stores
-    enable: false,
-    // If set to true this tab tries to immediately recover the shared state from another tab. Defaults to true.
-    initialize: true,
-    // Enforce a type. One of native, idb, localstorage or node. Defaults to native.
-    type: "localstorage",
-  })
-);
 
 const app = createApp(App);
 app.use(pinia);
