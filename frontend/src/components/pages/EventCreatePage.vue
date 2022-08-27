77487f19ec7b540e320a4d274833f4e341f785c7
Marco <marco.razor@hotmail.com>
2022-08-27 17:29:47 +0200

    added image input to event edit

--- frontend/src/components/pages/EventCreatePage.vue
+++ frontend/src/components/pages/EventCreatePage.vue
@@ -1,6 +1,33 @@
 <template>
   <teleport to="#nav-title">Create Event</teleport>
   <v-main>
-    <v-container> Create Event Page goes here. </v-container>
+    <v-container>
+      <edit-event v-model="matchyEvent" />
+    </v-container>
   </v-main>
 </template>
+
+<script lang="ts" setup>
+import type { EditEventInfo } from "@/services/eventService";
+import { Temporal } from "@js-temporal/polyfill";
+
+const matchyEvent = reactive({
+  title: "",
+  description: "",
+  header_image: undefined,
+  max_participants: 50,
+  datetime: Temporal.Now.zonedDateTimeISO(Temporal.Now.timeZone()),
+  location: "",
+  uses_groups: true,
+  event_groups: {
+    groupA: {
+      title: "",
+      description: "",
+    },
+    groupB: {
+      title: "",
+      description: "",
+    },
+  },
+} as EditEventInfo);
+</script>
