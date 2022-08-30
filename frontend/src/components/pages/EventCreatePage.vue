b3de982c1064926a80406bd74c061a72b685c6d9
Marco <marco.razor@hotmail.com>
2022-08-30 12:50:46 +0200

    improved readability

--- frontend/src/components/pages/EventCreatePage.vue
+++ frontend/src/components/pages/EventCreatePage.vue
@@ -20,12 +20,11 @@ const eventService = useEventService(useAuthStore());
 const submit = async () => {
   try {
     await eventService.createEvent(matchyEvent);
+    successToast(t("pages.event-create.success"));
   } catch (_e) {
     console.log(_e);
     errorToast(t("pages.event-create.error"));
-    return;
   }
-  successToast(t("pages.event-create.success"));
 };
 
 const matchyEvent = reactive({
