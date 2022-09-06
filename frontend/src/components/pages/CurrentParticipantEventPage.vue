fbfb79182290fe15766f74c37a4156baab0c9294
MJDeligan <marco.razor@hotmail.com>
2022-09-06 17:12:07 +0200

    add basic pair creation

--- frontend/src/components/pages/CurrentParticipantEventPage.vue
+++ frontend/src/components/pages/CurrentParticipantEventPage.vue
@@ -1,6 +1,6 @@
 <template>
   <v-main>
-    <v-container class="h-100">
+    <v-container class="h-100 pt-12">
       <div
         v-if="!eventStarted"
         class="h-50 d-flex flex-column align center justify-center pl-2"
@@ -12,10 +12,31 @@
           >Please wait for the organizer to start the event</span
         >
       </div>
-      <div class="h-100 d-flex justify-center align-center" v-if="eventEnded">
+      <div
+        class="h-100 d-flex justify-center align-center"
+        v-else-if="eventEnded"
+      >
         <span class="d-block">That's all volks!</span>
       </div>
-      <div v-else-if="roundOngoing">Current Round ID: {{ currentRoundId }}</div>
+      <div v-else-if="roundOngoing && !currentPairId">
+        <span class="d-block text-body-1 font-weight-bold"
+          >Let's find you a partner for this round!
+        </span>
+        <span class="d-block mb-4 text-body-2"
+          >Use the Qr-Scanner below or type their id in the textbox</span
+        >
+        <div
+          :style="{ width: '250px', height: '250px' }"
+          class="bg-grey mx-auto"
+        >
+          <span class="text-white">Nice Qr-Code scanner</span>
+        </div>
+        <v-text-field v-model="otherUser" label="ID (optional)">
+          <template v-slot:append>
+            <v-btn @click="createPair" color="secondary">Pair</v-btn>
+          </template>
+        </v-text-field>
+      </div>
       <div v-else>Waiting for new round...</div>
     </v-container>
   </v-main>
@@ -38,6 +59,19 @@ const eventEnded = ref(false);
 const roundOngoing = ref(false);
 const currentRoundId = ref<number>();
 const currentPairId = ref<number>();
+
+const otherUser = ref("");
+
+const createPair = async () => {
+  if (!currentRoundId.value) return;
+  try {
+    await currentEvent.createPair(otherUser.value, currentRoundId.value);
+    successToast;
+  } catch (e) {
+    console.log(e);
+    errorToast(e);
+  }
+};
 
 watch(
   () => eventStarted.value,
