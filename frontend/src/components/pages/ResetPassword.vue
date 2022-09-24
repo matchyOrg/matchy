817613e1739d142153f929b02ba05d3b1d4a5404
MJDeligan <marco.razor@hotmail.com>
2022-09-25 01:07:27 +0200

    extract hardcoded strings to i18n messages

--- frontend/src/components/pages/ResetPassword.vue
+++ frontend/src/components/pages/ResetPassword.vue
@@ -5,32 +5,30 @@
         <div class="d-flex align-center mb-6">
           <img class="mr-2" src="@/assets/matchyLogo.svg" height="24" />
           <span class="d-block text-h6 font-weight-bold">
-            Choose a new password
+            {{ t("pages.reset-password.heading-p1") }}
           </span>
         </div>
-        <span class="d-block"
-          >Keep in mind: Using a password manager will help you set up a super
-          secure password that you won't even need to remember. Pretty
-          convenient stuff.</span
-        >
+        <span class="d-block">{{ t("pages.reset-password.heading-p2") }}</span>
       </div>
       <v-form @submit.prevent="submit.handler">
         <v-text-field
           v-model="password"
           :type="showPW ? 'text' : 'password'"
-          label="Password"
-          placeholder="secret password"
+          :label="t('shared.auth.password')"
+          :placeholder="t('shared.auth.password-placeholder')"
           :append-icon="showPW ? 'mdi-eye' : 'mdi-eye-off'"
           @click:append="showPW = !showPW"
         />
         <v-text-field
           v-model="repeatPassword"
           :type="showRepeatPW ? 'text' : 'password'"
-          label="Repeat password"
-          placeholder="repeat secret password"
+          :label="t('shared.auth.repeat-password')"
+          :placeholder="t('shared.auth.repeat-password-placeholder')"
           :append-icon="showRepeatPW ? 'mdi-eye' : 'mdi-eye-off'"
           @click:append="showRepeatPW = !showRepeatPW"
-          :rules="[(val) => val === password || 'Passwords must match']"
+          :rules="[
+            (val) => val === password || t('shared.auth.password-mismatch'),
+          ]"
         />
         <v-btn
           :loading="submit.loading"
@@ -47,7 +45,7 @@
           <template v-slot:loader>
             <v-progress-circular indeterminate />
           </template>
-          Confirm</v-btn
+          {{ t("shared.forms.confirm") }}</v-btn
         >
       </v-form>
     </v-container>
@@ -55,8 +53,10 @@
 </template>
 <script lang="ts" setup>
 import { useAuthStore } from "@/stores/auth";
+import { useI18n } from "vue-i18n";
 
 const authStore = useAuthStore();
+const { t } = useI18n();
 const password = ref("");
 const repeatPassword = ref("");
 
