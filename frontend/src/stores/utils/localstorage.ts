e8ca69a5b8aa79c4eacdd019ecebb2c19d29b4b8
stefnotch <brandmairstefan@gmail.com>
2022-09-30 14:22:08 +0200

    Add unused localStorage wrapper with migrations

---
+++ frontend/src/stores/utils/localstorage.ts
@@ -0,0 +1,26 @@
+import type { Ref, ShallowRef } from "vue";
+import { ExtendedLocalStorage } from "./localstorage-json";
+import {
+  migrateToLatest,
+  type LocalStorageDatabase,
+} from "./localstorage-migrations";
+
+const storage = new ExtendedLocalStorage<LocalStorageDatabase>();
+
+const rawData = storage.readAll();
+const data = migrateToLatest(
+  rawData.version !== undefined ? (rawData as any) : { version: 0 }
+);
+storage.writeAll(data);
+
+export function storageRef<Key extends keyof LocalStorageDatabase>(key: Key) {
+  return computed<LocalStorageDatabase[Key]>({
+    get() {
+      return data[key];
+    },
+    set(value) {
+      data[key] = value;
+      storage.write(key, value);
+    },
+  });
+}
