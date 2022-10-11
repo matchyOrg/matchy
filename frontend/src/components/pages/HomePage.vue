bf72050509530b3a5fb549414afc1a614e3e84f0
Yahya Jabary <jabaryyahya@gmail.com>
2022-10-11 21:31:58 +0200

    fix parts of home page

--- frontend/src/components/pages/HomePage.vue
+++ frontend/src/components/pages/HomePage.vue
@@ -1,142 +1,157 @@
 <template>
   <v-main>
     <v-container>
-      <page-title>Matchy</page-title>
-      <h2
-        class="my-7 text-grey font-weight-regular"
-        v-if="authStore.isRegistered"
-      >
-        {{ t("pages.home.hello") }} {{ firstName }}!
-      </h2>
-      <div class="my-7 d-flex justify-center" v-else>
-        <v-btn to="/edit-profile">
-          {{ t("pages.home.finish-profile") }}
-        </v-btn>
-      </div>
+      <!-- Greeting -->
+      <section>
+        <h2
+          class="mt-1 mb-7 text-grey font-weight-regular"
+          v-if="authStore.isRegistered"
+        >
+          {{ t("pages.home.hello") }} {{ firstName }}!
+        </h2>
+        <div class="mt-5 mb-7 d-flex justify-center" v-else>
+          <v-btn to="/edit-profile">
+            {{ t("pages.home.finish-profile") }}
+          </v-btn>
+        </div>
+      </section>
+
+      <!-- Events currently running -->
+      <section>
+        <h2
+          v-if="currentUserEvents.length > 0"
+          class="text-h6 font-weight-bold mb-4"
+        >
+          {{ t("pages.home.confirm-presence-header") }}
+        </h2>
+        <event-list-item
+          class="mb-4"
+          v-for="(e, i) in currentUserEvents"
+          :key="i"
+          :matchy-event="e"
+          show-image
+          :pulse="e.id == +currentEventStore.getCurrentId()"
+          :to="'/events/' + e.id"
+          @share="share(e)"
+        >
+          <v-card-actions class="d-flex justify-center mh-0">
+            <v-btn
+              v-if="e.id == +currentEventStore.getCurrentId()"
+              :to="'/events/' + e.id + '/participant'"
+              color="primary"
+              >{{ t("pages.home.to-action") }}</v-btn
+            >
+            <v-btn
+              v-else
+              color="primary"
+              @click.prevent="confirmPresence(e.id)"
+              >{{ t("pages.home.confirm-presence-action") }}</v-btn
+            >
+          </v-card-actions>
+        </event-list-item>
+      </section>
 
       <!-- Participant view -->
-      <h2
-        v-if="currentUserEvents.length > 0"
-        class="text-h6 font-weight-bold mb-4"
-      >
-        {{ t("pages.home.confirm-presence-header") }}
-      </h2>
-      <event-list-item
-        class="mb-4"
-        v-for="(e, i) in currentUserEvents"
-        :key="i"
-        :matchy-event="e"
-        show-image
-        :pulse="e.id == +currentEventStore.getCurrentId()"
-        :to="'/events/' + e.id"
-        @share="share(e)"
-      >
-        <v-card-actions class="d-flex justify-center mh-0">
-          <v-btn
-            v-if="e.id == +currentEventStore.getCurrentId()"
-            :to="'/events/' + e.id + '/participant'"
-            color="primary"
-            >{{ t("pages.home.to-action") }}</v-btn
-          >
-          <v-btn
-            v-else
-            color="primary"
-            @click.prevent="confirmPresence(e.id)"
-            >{{ t("pages.home.confirm-presence-action") }}</v-btn
-          >
-        </v-card-actions>
-      </event-list-item>
-      <v-spacer />
-      <h2 class="text-h6 font-weight-bold mb-4">
-        {{ t("pages.home.future-events-header") }}
-      </h2>
-      <template v-if="futureUserEvents.length > 0">
-        <event-list-item
-          class="mb-4"
-          v-for="(e, i) in futureUserEvents"
-          :key="i"
-          :matchy-event="e"
-          show-info
-          :to="'/events/' + e.id"
-          @share="share(e)"
-        />
-      </template>
-      <div v-else class="text-center text-grey">
-        {{ t("pages.home.no-events") }}
-        <br />
-        <v-btn color="primary" variant="text" class="mx-auto" to="/events">{{
-          t("pages.home.no-event-cta")
-        }}</v-btn>
-      </div>
+      <section>
+        <h2 class="text-h6 font-weight-bold mb-4">
+          {{ t("pages.home.future-events-visiting-header") }}
+        </h2>
+        <template v-if="futureUserEvents.length > 0">
+          <event-list-item
+            class="mb-4"
+            v-for="(e, i) in futureUserEvents"
+            :key="i"
+            :matchy-event="e"
+            show-info
+            :to="'/events/' + e.id"
+            @share="share(e)"
+          />
+        </template>
+        <div v-else class="text-center text-grey">
+          {{ t("pages.home.no-events") }}
+          <br />
+          <v-btn color="primary" variant="text" class="mx-auto" to="/events">{{
+            t("pages.home.no-event-cta")
+          }}</v-btn>
+        </div>
+      </section>
 
-      <!-- Organiser events: -->
-      <div
-        v-if="currentOrganizerEvents.length > 0"
-        class="text-h6 font-weight-bold mb-4"
-      >
-        {{ t("pages.home.active-event") }}
-      </div>
-      <event-list-item
-        class="mb-4"
-        v-for="(e, i) in currentOrganizerEvents"
-        :key="i"
-        :matchy-event="e"
-        show-image
-        :to="'/events/' + e.id"
-        :pulse="e.id == +currentEventStore.getCurrentId()"
-        @share="share(e)"
-      >
-        <v-card-actions class="d-flex justify-center mh-0">
-          <v-btn
-            v-if="e.id == +currentEventStore.getCurrentId()"
-            :to="'/events/' + e.id + '/dashboard'"
-            color="primary"
-            >{{ t("pages.home.active-event-action") }}</v-btn
-          >
-          <v-btn v-else color="primary" @click.prevent="startEvent(e.id)"
-            >Start Event</v-btn
-          >
-        </v-card-actions>
-      </event-list-item>
-      <v-spacer />
-      <div class="text-h6 font-weight-bold mb-4">
-        {{ t("pages.home.future-events-header") }}
-      </div>
-      <template v-if="futureOrganizerEvents.length > 0">
+      <!-- Organizer events -->
+      <section>
+        <div
+          v-if="currentOrganizerEvents.length > 0"
+          class="text-h6 font-weight-bold mb-4"
+        >
+          {{ t("pages.home.active-event") }}
+        </div>
         <event-list-item
           class="mb-4"
-          v-for="(e, i) in futureOrganizerEvents"
+          v-for="(e, i) in currentOrganizerEvents"
           :key="i"
           :matchy-event="e"
-          show-info
+          show-image
           :to="'/events/' + e.id"
+          :pulse="e.id == +currentEventStore.getCurrentId()"
           @share="share(e)"
-        />
-      </template>
-      <div v-else class="text-center text-grey">
-        {{ t("pages.home.no-org-events") }}
-        <v-btn
-          color="primary"
-          variant="text"
-          class="mx-auto"
-          to="/create-event"
         >
-          {{ t("pages.home.no-org-events-cta") }}
-        </v-btn>
-      </div>
+          <v-card-actions class="d-flex justify-center mh-0">
+            <v-btn
+              v-if="e.id == +currentEventStore.getCurrentId()"
+              :to="'/events/' + e.id + '/dashboard'"
+              color="primary"
+              >{{ t("pages.home.active-event-action") }}</v-btn
+            >
+            <v-btn v-else color="primary" @click.prevent="startEvent(e.id)"
+              >Start Event</v-btn
+            >
+          </v-card-actions>
+        </event-list-item>
+
+        <div class="text-h6 font-weight-bold mb-4">
+          {{ t("pages.home.future-events-hosting-header") }}
+        </div>
+        <template v-if="futureOrganizerEvents.length > 0">
+          <event-list-item
+            class="mb-4"
+            v-for="(e, i) in futureOrganizerEvents"
+            :key="i"
+            :matchy-event="e"
+            show-info
+            :to="'/events/' + e.id"
+            @share="share(e)"
+          />
+        </template>
+        <div v-else class="text-center text-grey">
+          {{ t("pages.home.no-org-events") }}
+          <v-btn
+            color="primary"
+            variant="text"
+            class="mx-auto"
+            to="/create-event"
+          >
+            {{ t("pages.home.no-org-events-cta") }}
+          </v-btn>
+        </div>
+      </section>
     </v-container>
   </v-main>
-  <v-footer class="d-flex justify-center pb-4">
-    <!-- other links -->
+
+  <v-footer class="flex justify-center -mb-21">
     <a
       href="https://github.com/matchyOrg/.github/blob/main/profile/README.md"
-      class="mx-4 text-grey"
+      class="mx-2 text-grey"
     >
       {{ t("pages.login.footer-about-us") }}
     </a>
     <a
+      href="https://docs.google.com/forms/d/e/1FAIpQLSfadw82Hn0h0E6R8vLdCsXYoEZX5-ys59SOSx1UMEuhp6K-Tg/viewform?usp=sf_link"
+      class="mx-2 text-grey"
+    >
+      {{ t("pages.login.footer-feedback") }}
+    </a>
+    <a
       href="https://github.com/matchyOrg/matchy/blob/main/TERMS.md"
-      class="mx-4 text-grey"
+      class="mx-2 text-grey"
     >
       {{ t("pages.login.footer-legal-notice") }}
     </a>
